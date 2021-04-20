cd5596cefde9d7753f9b5509b97363f52ffa9714
Sumit Rajegore <sumitrajegorevp@gmail.com>
2021-04-20 15:50:34 +0530

    initialize stack and betting value

---
+++ Gambler.js
@@ -0,0 +1,6 @@
+console.log('Welcome to my Gamblar simulaton problem');
+//Constants
+const STACK_AMOUNT = 100 ;
+const BETTING_AMOUNT = 1 ;
+console.log(STACK_AMOUNT);
+console.log(BETTING_AMOUNT);
